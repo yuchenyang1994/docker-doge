71fdba6370fa1a26420b4b0296b16c195234bd14
yuchenyang1994 <yuchenyang1994@foxmail.com>
2017-09-10 13:38:43 +0800

    add:增加权限模型配置文件

---
+++ middleware/auth.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"sync"
+
+	"github.com/casbin/casbin"
+	"github.com/casbin/gorm-adapter"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var (
+	enforcer *casbin.Enforcer
+	once     sync.Once
+)
+
+// get Auth 单例模式返回权限校验器
+func GetAuthzInstance() *casbin.Enforcer {
+	once.Do(
+		func() {
+			adpter := gormadapter.NewAdapter("sqlite3", "../data.db", true)
+			enforcer = casbin.NewEnforcer("./configs/authz_model.conf", adpter)
+		})
+	return enforcer
+}
