1df2b2b470873c1e3d570e737cb97cf3487f94ee
chenyang.yu <chenyang.yu@hypers.com>
2017-09-25 18:05:33 +0800

    feat：增加一些命令行，方便后续开发

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"docker-doge/db"
 	"docker-doge/middleware"
 
+	"fmt"
 	"github.com/gin-contrib/authz"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"os"
 )
 
-func main() {
-	d := db.GetDbInstance()
-	db.MigrationDB(d)
+// run ...
+func runServer() {
 	e := middleware.GetAuthzInstance()
 	r := gin.New()
 	r.Use(gin.Logger())   // 日志处理
@@ -23,4 +24,37 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+// migrate ...
+func migrate() {
+	d := db.GetDbInstance()
+	db.MigrationDB(d)
+}
+
+// createRole ...
+func createRole() {
+	d := db.GetDbInstance()
+	db.CreateRole(d)
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		arg := os.Args[1]
+		fmt.Println(arg)
+		switch arg {
+		case "runserver":
+			runServer()
+		case "migrate":
+			migrate()
+		case "createrole":
+			createRole()
+		}
+	} else {
+		fmt.Println("USAGE:",
+			"runserver: 启动服务器",
+			"migrate: 同步表结构",
+			"createrole: 插入初始用户角色")
+	}
+
 }
